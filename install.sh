82f84c05b87d12cb7834f6bd82d03020f68369e2
Alladio Bonesso <Alladio@users.noreply.github.com>
2023-05-10 07:46:50 -0300

    loggenerator

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
-sudo yum install aws-kinesis
+#install Kinesis
+sudo yum install aws-kinesis-agent
+
+#diretório Kinesis
+cd /etc/aws-kinesis/
+#config-file Kenesis agent
+sudo nano agent.json
+
+#config your kinesis.endpoint or firehse.endpoint
+#config your IAM: "awsAccessKeyId":"", "awsSecretAccessKey":""
+
+#or MORE security
+#Create new IAM ROLE: EC2>AdministratorAccess
+#RoleName: "EC2 "AdminAccess"
+#Create Role
+
+sudo service aws-kinesis-agent start
+sudo chkconfig aws-kinesis-agent on
